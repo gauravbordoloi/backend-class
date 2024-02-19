3745428b8425cff9d35f65fc25d4eba261f0adb8
ibleedcode86 <115789805+ibleedcode86@users.noreply.github.com>
2024-02-19 18:53:23 +0530

    new prob

--- students/bibek/index.js
+++ students/bibek/index.js
@@ -1,3 +1,15 @@
-for(var i=100;i>=0;i--){
-  console.log(i)
+// for(var i=100;i>=0;i--){
+//   console.log(i)
+// }
+ 
+
+var sum = 0
+for (let i = 1; i <= 500; i++){
+    sum += i
+    if(sum >= 500) {
+        break;
+    }
 }
+    
+
+console.log(sum)
