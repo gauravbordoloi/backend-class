87f2a89508b5e486d5dd7c141e9d14a163d63d4f
ibleedcode86 <115789805+ibleedcode86@users.noreply.github.com>
2024-02-20 22:03:25 +0530

    practise bibek

--- students/bibek/index.js
+++ students/bibek/index.js
@@ -3,13 +3,38 @@
 // }
  
 
-var sum = 0
-for (let i = 1; i <= 500; i++){
-    sum += i
-    if(sum >= 500) {
-        break;
-    }
-}
+// var sum = 0
+// for (let i = 1; i <= 500; i++){
+//    sum += i
+//     if(sum >= 500) {
+//         break;
+//     }
+// }
     
+// console.log(sum)
 
-console.log(sum)
+// var no1 = 1
+
+// for (let i = 1; i <= 50; i++)(
+//     sum += i
+//     if(no1 >= 50) {
+//         break;
+//     }
+// )
+
+// console.log(no1)
+
+
+
+
+
+//tableau//
+
+// let name = "bibek deka"
+// for (let i = 0; i < name.length; i++) {
+//     console.log[i];
+    
+// }
+
+
+
---
+++ students/bibek/main.js
@@ -0,0 +1,9 @@
+/* let bibek= {
+   firstName : "Bibek",
+   lastName: "Deka",
+   class: 12,
+}
+
+console.log(bibek.class) */
+
+const iliyas = [ 12,54,36]
