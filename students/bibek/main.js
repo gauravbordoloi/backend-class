f63fcdb69bfdd97640ccab7019e62bdcdcb89d25
ibleedcode86 <115789805+ibleedcode86@users.noreply.github.com>
2024-02-20 22:21:01 +0530

    pop array

--- students/bibek/main.js
+++ students/bibek/main.js
@@ -6,4 +6,14 @@
 
 console.log(bibek.class) */
 
-const iliyas = [ 12,54,36]
+// const iliyas = [ 12,54,36]
+
+// pop.iliyas
+
+// console.log(iliyas)
+
+const fruits = ['apple', 'banana', 'orange'];
+const removedFruit = fruits.pop();
+
+console.log(removedFruit); 
+console.log(fruits); 
