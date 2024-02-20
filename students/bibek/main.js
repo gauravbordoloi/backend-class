e9eb01a1949ead425859beaaf252fb43fac1d70f
ibleedcode86 <115789805+ibleedcode86@users.noreply.github.com>
2024-02-20 22:31:32 +0530

    pop push

--- students/bibek/main.js
+++ students/bibek/main.js
@@ -12,8 +12,21 @@ console.log(bibek.class) */
 
 // console.log(iliyas)
 
-const fruits = ['apple', 'banana', 'orange'];
-const removedFruit = fruits.pop();
+// const fruits = ['apple', 'banana', 'orange'];
+// const removedFruit = fruits.pop();
 
-console.log(removedFruit); 
-console.log(fruits); 
+// console.log(removedFruit); 
+// console.log(fruits); 
+
+// var fruits = ['apple', 'banana', 'orange'];
+// fruits push(jam);
+
+// console.log(fruits); 
+
+const fruits = ['apple', 'banana'];
+// const removedFruit = fruits.pop();
+fruits.push('orange');
+
+
+console.log(fruits);
+// console.log(removedFruit);
