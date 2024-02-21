b89645f7e5e3fce73422498193bee6611ae42291
ibleedcode86 <115789805+ibleedcode86@users.noreply.github.com>
2024-02-21 18:47:23 +0530

    timer n reversed

---
+++ students/bibek/main.js
@@ -0,0 +1,34 @@
+// let str = "i.like.this.program.very.much";
+// let reversed = '';
+// let bdk = str.split (".");
+// for (let i = bdk.length-1; i >= 0; i--) {
+//   reversed += (bdk[i] + ".");
+// //   reversed += "."; 
+// }
+// console.log(reversed);
+// let new = "i.like.this.program.very.much";
+// let newstr = "";
+// let seperate = new.split(".");
+// for (let i = seperate.length - 1; i >= 0; i--) {
+//     if (i != seperate.length - 1) {
+//         newstr = newstr + seperate[i] + ".";
+//     } else {
+//         newstr = newstr + seperate[i];
+//     }
+// }
+// console.log(newstr);
+
+// let str = "my.name.is.bibek.deka";
+// let reversed = "";
+// let bdk = str.split (".");
+// for (let i = bdk.length; i >=0; i--) {
+//    reversed += (bdk[i] + ".");
+// }
+
+// console.log(reversed)
+
+setTimeout(() => {
+   console.log(2 sec is over);
+}, 1000);
+
+console.log(1 sec is over)
