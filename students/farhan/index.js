b14fd217b8bd88421369eddd7139274db58bdfef
Farhan Hussain <121046303+princeofficial355@users.noreply.github.com>
2024-02-21 17:17:32 +0530

    MESSAGE

--- students/farhan/index.js
+++ students/farhan/index.js
@@ -1,3 +1,14 @@
-for(let i = 100; i >= 0; i--){
-    console.log(i)
+// Reverse words in a given string
+
+
+var s = "i.like.this.program.very.much"
+
+var s2 = s.split(".")
+
+var s1 = ""
+for (let i = s2.length-1; i >= 0; i--) {
+    s1 += s2[i]  
+    s1 += "."
 }
+
+console.log(s1)
